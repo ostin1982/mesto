b26c7c417588c9dbb556115d49c0a14a05884a8e
ostin1982 <ostin1982@yandex.ru>
2020-09-03 13:04:34 +0300

    Второй коммит. Четвёртый спринт

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,40 @@
-//вызвать popup
-
 let popup = document.querySelector('.popup');
-let popupOpenButton = document.querySelector('.js-popup__open');
-let popupCloseButton = popup.querySelector('.js-popup__close');
-
-let popupToggle = function() {
-    popup.classList.toggle('popup_is-open')
-}
-
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
-
-//Поменять Имя и Род интересов
-
+let popupOpenButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = popup.querySelector('.popup__close');
 let name = document.querySelector('.profile__name');
 let occupation = document.querySelector('.profile__occupation');
-let popupName = document.querySelector('.popup__name');
-let popupOccupation = document.querySelector('.popup__occupation');
+let popupName = document.querySelector('.popup__about_name');
+let popupOccupation = document.querySelector('.popup__about_occupation');
 let popupSubmitButton = document.querySelector('.popup__submit');
+let popupInput = document.querySelector('.popup__input');
 
+
+//вызвать popup
+function popupToggle () {
+    popup.classList.toggle('popup_is-open');
+};
+
+
+//поменять Имя и Род интересов
 function newName (evt) {
     evt.preventDefault();
 
     name.textContent = popupName.value;
-}
+    occupation.innerText = popupOccupation.value;
+};
 
 
-popupSubmitButton.addEventListener('submit', newName);
-popupSubmitButton.addEventListener('click', newName);
-popupSubmitButton.addEventListener('click', popupToggle);
-
-
-
-function newOccupation (evt) {
+//новое имя в графу popup
+function about (evt) {
     evt.preventDefault();
 
-    occupation.innerText = popupOccupation.value;
-}
+    popupName.innerText = name.value;
+    popupOccupation.innerText = occupation.value;
+};
 
 
-popupSubmitButton.addEventListener('submit', newOccupation);
-popupSubmitButton.addEventListener('click', newOccupation);
+popupOpenButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
+popupInput.addEventListener('submit', newName);
+popupSubmitButton.addEventListener('click', newName);
 popupSubmitButton.addEventListener('click', popupToggle);
-
