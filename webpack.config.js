2664758194526cfc8ea966b8e94beaa44faad304
ostin1982 <68117950+ostin1982@users.noreply.github.com>
2020-11-09 14:06:48 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,71 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
-
 
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
+    entry: './src/pages/index.js',
     output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
     },
-    mode: 'development',
+
+    devtool: 'inline-source-map',
     devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
         open: true,
         compress: true,
-
-    },
-    
-    devtool: 'inline-source-map',
-    
+        hot: true,
+        port: 8080,
+        overlay: true,
+        },
+        
 
         module: {
             rules: [
                 {
-                    test: /\.js$/,
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
                     loader: 'babel-loader',
-                    exclude: '/node_modules/'
+                    options: {
+                    presets: ['@babel/preset-env']
+                        },
+                    },
                 },
                 {
-                    test: /\.(png|svg|jpg|gif|woff2)$/,
-                    loader: 'file-loader'
+                    test: /\.(png|jpe?g|gif|svg|woff|woff2)$/i,
+                    use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                    ],
                 },
                 {
-                    test: /\.html$/,
+                    test: /\.html$/i,
                     loader: 'html-loader',
                 },
                 {
-                    test: /\.css$/,
-                    loader: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1
-                        }
-                    },
-                    'postcss-loader'
-                    ],
-                },  
-            ]
+                    test: /\.css$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                    importLoaders: 1,
+                            },
+                        },
+                        'postcss-loader'
+                        ],
+                },
+                ],
         },
-
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './index.html'
-            }),
-            new MiniCssExtractPlugin(),
-            new CleanWebpackPlugin(),
-            new webpack.HotModuleReplacementPlugin(),
-        ],
-}
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin(),
+    ]
+};
